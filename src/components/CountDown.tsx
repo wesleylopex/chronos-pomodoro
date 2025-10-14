11dc5a331b486ef82297ec70a9e31037012913d9
Wesley Lopes de Oliveira <x.wesleylopes@gmail.com>
2025-10-14 09:27:14 -0300

    fix: revert to static timer display in CountDown component

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -1,10 +1,7 @@
-import { useTaskContext } from '@/contexts/TaskContext/useTaskContext'
-
 export default function CountDown() {
-  const { state } = useTaskContext()
   return (
     <h2 className="text-center text-6xl md:text-8xl lg:text-8xl font-bold">
-      {state.formattedSecondsRemaining}
+      25:00
     </h2>
   )
 }
