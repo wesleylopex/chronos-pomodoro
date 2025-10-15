afbf6289625ce76802e9e7aa26dfb349cb2f520b
Wesley Lopes de Oliveira <x.wesleylopes@gmail.com>
2025-10-15 17:57:44 -0300

    feat: enhance TaskContextProvider to initialize state from localStorage and reset task values

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -5,9 +5,23 @@ import { taskReducer } from './taskReducer'
 import { TimerWorkerManager } from '@/workers/TimerWorkerManager'
 import { TaskActionTypes } from './task-actions'
 import { loadBeep } from '@/utils/load-beep'
+import type { TaskState } from '@/types/task-state'
 
 export function TaskContextProvider({ children }: { children: React.ReactNode }) {
-  const [state, dispatch] = useReducer(taskReducer,initialTaskState)
+  const [state, dispatch] = useReducer(taskReducer, initialTaskState, () => {
+    const localData = localStorage.getItem('state') || null
+
+    if (!localData) return initialTaskState
+
+    const parsedData = JSON.parse(localData) as TaskState
+
+    return {
+      ...parsedData,
+      activeTask: null,
+      secondsRemaining: 0,
+      formattedSecondsRemaining: '00:00'
+    }
+  })
   const playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null)
 
   const worker = TimerWorkerManager.getInstance()
@@ -34,6 +48,8 @@ export function TaskContextProvider({ children }: { children: React.ReactNode })
   })
 
   useEffect(() => {
+    localStorage.setItem('state', JSON.stringify(state))
+
     if (!state.activeTask) {
       worker.terminate()
     }
